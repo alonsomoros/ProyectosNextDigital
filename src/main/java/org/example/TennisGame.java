bac4740aaecd62800676c3ac9875a609b3222572
Alon <alonsomorosvillalba@gmail.com>
2025-01-30 01:29:24 +0100

    Commit de las clases de Test y Modelo

---
+++ src/main/java/org/example/TennisGame.java
@@ -0,0 +1,69 @@
+package org.example;
+
+import java.util.Map;
+
+public class TennisGame {
+
+    Map<Integer, String> scoring = Map.of(0, "Love", 1, "Fifteen", 2, "Thirty",3,"Fourty");
+
+
+    private int scorePlayer1;
+    private int scorePlayer2;
+
+    public TennisGame() {
+        this.scorePlayer1 = 0;
+        this.scorePlayer2 = 0;
+    }
+
+    public String getScore() {
+        String score = "";
+        if (scorePlayer1 == scorePlayer2) {
+            score = checkSameResult();
+        } else {
+            score = checkDifferentResult();
+        }
+        return score;
+    }
+
+    private String checkDifferentResult() {
+        String score = "";
+        if (scorePlayer1 > 3 || scorePlayer2 > 3) {
+            if (scorePlayer1 - scorePlayer2 == 1) {
+                score = "Advantage Player 1";
+            } else if (scorePlayer2 - scorePlayer1 == 1) { // Tanto si es 4-1 como 4-2 no sería ventaja sería win
+                score = "Advantage Player 2";
+            } else if (scorePlayer1 - scorePlayer2 >= 2) {
+                score = "Player 1 Wins";
+            } else {
+                score = "Player 2 Wins";
+            }
+        } else {
+            score = scoring.get(scorePlayer1) + "-" + scoring.get(scorePlayer2);
+        }
+        return score;
+    }
+
+    private String checkSameResult() {
+        String score = "";
+        if (scorePlayer1 == 0){
+            score = "Love-Love";
+        } else if (scorePlayer1 == 3) {
+            score = "Deuce";
+        } else {
+            score = scoring.get(scorePlayer1) + "-All";
+        }
+        return score;
+    }
+
+    public void playerOneScores(){
+        if (scorePlayer2 == 4){
+            this.scorePlayer2--;
+        } else this.scorePlayer1++;
+    }
+
+    public void playerTwoScores(){
+        if(scorePlayer1 == 4){
+            this.scorePlayer1--;
+        } else this.scorePlayer2++;
+    }
+}
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,228 @@
 package org.example;
 
-import junit.framework.Test;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+public class AppTest extends TestCase {
+
+    private TennisGame tennisGame;
+
+    @BeforeEach
+    public void setUp() {
+        tennisGame = new TennisGame();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    // Basic Scoring
+
+    @Test
+    public void shouldReturnLoveLove_OnStart(){
+        String score = tennisGame.getScore();
+
+        assertEquals("Love-Love",score);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testPlayerOneScoresOnce_ShouldBeFifteenLove(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerOneScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Fifteen-Love",score);
     }
+
+    @Test
+    public void testPlayerTwoScoresOnce_ShouldBeLoveFifteen(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerTwoScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Love-Fifteen",score);
+    }
+
+    @Test
+    public void testBothPlayersScoreOnce_ShouldBeFifteenAll(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerOneScores();
+
+        tennisGame.playerTwoScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Fifteen-All",score);
+    }
+
+    @Test
+    public void testPlayerOneScoresTwice_ShouldBeThirtyLove(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Thirty-Love",score);
+    }
+
+    @Test
+    public void testPlayerTwoScoresTwice_ShouldBeLoveThirty() {
+        tennisGame = new TennisGame();
+
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Love-Thirty", score);
+    }
+
+    // Deuce and Advantage
+
+    @Test
+    public void testScoreIsFortyAll_ShouldBeDeuce(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Deuce",score);
+    }
+
+    @Test
+    public void testPlayerOneAdvantageAfterDeuce_ShouldBeAdvantagePlayerOne(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+
+        tennisGame.playerOneScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Advantage Player 1",score);
+    }
+
+    @Test
+    public void testPlayerTwoAdvantageAfterDeuce_ShouldBeAdvantagePlayerTwo(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Advantage Player 2",score);
+    }
+
+    @Test
+    public void testPlayerOneWinsAfterAdvantage_ShouldBeWinPlayerOne(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Player 1 Wins",score);
+    }
+
+    @Test
+    public void testPlayerTwoWinsAfterAdvantage_ShouldBeWinPlayerTwo(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Player 2 Wins",score);
+    }
+
+    @Test
+    public void testAdvantageLost_BackToDeuce(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+
+        tennisGame.playerOneScores();
+
+        tennisGame.playerTwoScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Deuce",score);
+    }
+
+    // Winning the game
+
+    @Test
+    public void testPlayerOneWinsByTwoPoints_ShouldBeWinPlayerOne(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Player 1 Wins",score);
+    }
+
+    @Test
+    public void testPlayerTwoWinsByTwoPoints_ShouldBeWinPlayerTwo(){
+        tennisGame = new TennisGame();
+
+        tennisGame.playerOneScores();
+        tennisGame.playerOneScores();
+
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+        tennisGame.playerTwoScores();
+
+        String score = tennisGame.getScore();
+        assertEquals("Player 2 Wins",score);
+    }
+
+
 }
